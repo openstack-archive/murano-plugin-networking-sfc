08a6701f66996b3157fe15c6ee4f6909912c08a2
Peter Zhurba <pzhurba@mirantis.com>
2016-07-15 09:37:03 +0000

    some some some
    Change-Id: Ic79d2a6c79e2a3cd0e65edf560114064f939abb6

---
+++ example/SfcDemoApp/Resources/scripts/runSFCDemoDeploy.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+#  Licensed under the Apache License, Version 2.0 (the "License"); you may
+#  not use this file except in compliance with the License. You may obtain
+#  a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#  License for the specific language governing permissions and limitations
+#  under the License.
+
+srv(){
+
+    sudo dd of=/etc/init/sfc-generator.conf <<EOF
+# SFC Demo generator
+#
+
+description  "SFC Demo generator"
+
+start on runlevel [2345]
+stop on runlevel [!2345]
+
+
+script
+    while true ;  do
+        dmesg -T true | nc $1 8082 || true
+    done
+end script
+EOF
+    sudo initctl start sfc-generator
+}
+
+
+rcv(){
+
+    sudo dd of=/etc/init/sfc-receiver.conf <<EOF
+# SFC Demo receiver
+#
+
+description  "SFC Demo receiver"
+
+start on runlevel [2345]
+stop on runlevel [!2345]
+
+script
+    while true ;  do
+        nc -l 8082 >/dev/null || true
+    done
+end script
+EOF
+    sudo initctl start sfc-receiver
+
+}
+
+sfc(){
+    sudo apt-get -y install debconf-utils
+    echo "ntop ntop/admin_password_again password r00tme
+ntop ntop/admin_password password r00tme
+ntop ntop/user string ntop
+ntop ntop/interfaces string eth0
+" | sudo debconf-set-selections
+    sudo apt-get -y install ntop
+
+}
+
+set -x
+
+echo "$@" > /tmp/tst
+
+sudo apt-get update
+case "$1" in
+    "sfc-app" )
+        sfc 
+    ;;
+    "left-endpoint" )
+        rcv
+    ;;
+    "right-endpoint" )
+        srv $3
+esac
+
+
